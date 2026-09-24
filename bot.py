c4c73a6e773644ba60f002ac6a89c9d70ff65c50
joseortizromero2018-lang <joseortizromero2018@gmail.com>
2026-09-24 16:16:13 -0500

    Implement Flask server and Telegram bot for predictions
    Added Flask server and Telegram bot functionality for football predictions.

--- bot.py
+++ bot.py
@@ -39,6 +39,8 @@ def run_flask():
 API_KEY = "40f348e5bd5646daa606c0096cb13fb2"
 TELEGRAM_TOKEN = "8847776327:AAFfy6zP0uS1EwrXbgeR2WlJTNLul7gd50Q"
 
+bot.delete_webhook()
+
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 HEADERS = {"X-Auth-Token": API_KEY}
 SEASONS_TO_FETCH = [2023, 2024, 2025, 2026]
